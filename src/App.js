571a65241669bf9bb1103587066139ef23546e16
Tomaž Sagaj <tomaz.sagaj@gmail.com>
2023-11-27 10:24:57 +0100

    Project setup.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1 className="text-6xl text-red-500 bg-purple-200">Hello world</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
