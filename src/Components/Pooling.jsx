f085ca66e96060b53ee55aaad99fbc7abca88c56
kkeroo <61207502+kkeroo@users.noreply.github.com>
2023-12-05 13:00:44 +0100

    Connection to API added.

---
+++ src/Components/Pooling.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import ReactPolling from 'react-polling';
+
+export default function Pooling({ taskId, handleResults }) {
+    let url = 'http://localhost:8000/progress/'+taskId
+    return (
+        <>
+            { taskId !== '' &&
+                <ReactPolling
+                    url={url}
+                    interval= {3000} // in milliseconds(ms)
+                    retryCount={3} // this is optional
+                    onSuccess={(res) => {
+                        console.log(res)
+                        if (res.task_status === 'Running' && res.images.length == 0) {
+                            return true
+                        }
+                        else if (res.task_status === 'Completed' && res.images.length > 0) {
+                            handleResults(res)
+                            return false
+                        }
+                        return true
+                        
+                    }}
+                    onFailure={() => console.log('handle failure')} // this is optional
+                    method={'GET'}
+                    // headers={headers object} // this is optional
+                    // body={JSON.stringify(data)} // data to send in a post call. Should be stringified always
+                />
+            }
+        </>
+    )
+}
--- src/UI/Button.jsx
+++ src/UI/Button.jsx
@@ -1,7 +1,7 @@
 export default function Button(props) {
     return (
         <button className={"bg-white hover:bg-gray-400 text-gray-900 hover:text-white transition font-semibold py-2 px-4 border border-gray-400 rounded shadow" + " " + props.class} onClick={() => {props.onClick()}}>
-            {props.title}
+            {props.icon}{props.title}
         </button>
     );
 }
